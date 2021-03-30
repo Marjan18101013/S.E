a8dbc7beae01bf4531b3dd26ea56311b81eccbd8
Marjan18101013 <63208270+Marjan18101013@users.noreply.github.com>
2021-03-30 21:28:24 +0600

    done

---
+++ Home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def showhome(request):
+    return render(request, 'Home/home.html')
+
+def showabout(request):
+    return render(request, 'Home/about.html')
+
